8c8dc100220f45cac82dd49c21980667048fcb19
Agath21 <agathe.adam@jdev.fr>
2026-01-21 15:43:46 +0100

    Update custom

--- ecosante/customlayers/projets_eco.js
+++ ecosante/customlayers/projets_eco.js
@@ -12,21 +12,11 @@
   
   const ICON_SRC = "apps/omees/ecosante/img/marker_projet.svg";
  
-  const ICON_STYLE = new ol.style.Style({
+  const defaultStyle = new ol.style.Style({
     image: new ol.style.Icon({
       src: ICON_SRC,
       scale: 0.25,
     }),
-  });
-
-  const LINE_STYLE = new ol.style.Style({
-    stroke: new ol.style.Stroke({
-      color: "rgba(19, 62, 95, 1)",
-      width: 3,
-    }),
-  });
-
-  const POLY_STYLE = new ol.style.Style({
     stroke: new ol.style.Stroke({
       color: "rgba(19, 62, 95, 1)",
       width: 2,
@@ -35,23 +25,6 @@
       color: "rgba(19, 62, 95, 0.05)",
     }),
   });
-
-  const defaultStyle = function (feature) {
-    const geom = feature.getGeometry();
-    if (!geom) return null;
-
-    const type = geom.getType();
-
-    if (type === "Point" || type === "MultiPoint") return ICON_STYLE;
-    if (type === "LineString" || type === "MultiLineString") return LINE_STYLE;
-    if (type === "Polygon" || type === "MultiPolygon") return POLY_STYLE;
-
-    if (type === "GeometryCollection") {
-      return [ICON_STYLE, LINE_STYLE, POLY_STYLE];
-    }
-
-    return null;
-  };
 
   const layer = new ol.layer.Vector({
     source: new ol.source.Vector({
